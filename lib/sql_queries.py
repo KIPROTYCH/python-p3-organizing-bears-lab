d9ab7b106f7ead77a766444dc2c6af2d6c94d8ce
KIPROTYCH <kiprotych@gmail.com>
2023-08-24 23:26:03 +0300

    Completed Assignment

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -4,14 +4,12 @@ select_all_female_bears_return_name_and_age = """
        FROM bears
        WHERE sex = 'F';
 """
-
 select_all_bears_names_and_orders_in_alphabetical_order = """
     SELECT
      bears.name
     FROM bears
     ORDER BY name ASC
 """
-
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT
      bears.name,bears.age
